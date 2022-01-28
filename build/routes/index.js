6d08d9925d77951b88aa7441fd2a36f979485943
Abdelrahman Alaa <abdelrahmanalaa139@gmail.com>
2022-01-28 20:54:40 +0200

    edit .gitignore

--- build/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var imgListRouter_1 = require("./api/imgListRouter");
-var express = require("express");
-var imgRouter_1 = require("./api/imgRouter");
-var routes = express.Router();
-routes.use("/imgList", imgListRouter_1.default);
-routes.use("/imgs", imgRouter_1.default);
-exports.default = routes;
